0e6f52213ddb2db8cbd95ce26b63f1ba31b544bd
Alex Geine <mrborisov00@gmail.com>
2025-10-22 00:32:40 +0300

    Update TCP server/client

--- TCP/client/client.cpp
+++ TCP/client/client.cpp
@@ -1,20 +1,128 @@
 #include <iostream>
 #include <cstring>
+#include <cstdlib>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <config.hpp>
 #include <thread>
+#include <atomic>
+#include <poll.h>
+#include <csignal>
 
 using namespace std;
 
-int connect_to_server(uint16_t port, const string &ip)
-{
+atomic<bool> running{true};
+
+void signal_handler(int sig) {
+    running = false;
+    cout << "\nЗавершение работы...\n";
+}
+
+// Функция для установки таймаута
+bool set_socket_timeout(int sockfd, int seconds) {
+    struct timeval timeout;
+    timeout.tv_sec = seconds;
+    timeout.tv_usec = 0;
+    
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0) {
+        perror("setsockopt SO_RCVTIMEO failed");
+        return false;
+    }
+    
+    if (setsockopt(sockfd, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(timeout)) < 0) {
+        perror("setsockopt SO_SNDTIMEO failed");
+        return false;
+    }
+    
+    return true;
+}
+
+// Безопасная отправка
+bool safe_send(int socket, const string& message, int timeout_sec = 5) {
+    if (!set_socket_timeout(socket, timeout_sec)) {
+        return false;
+    }
+    
+    size_t total_sent = 0;
+    while (total_sent < message.length() && running) {
+        ssize_t sent = send(socket, message.c_str() + total_sent, 
+                           message.length() - total_sent, MSG_NOSIGNAL);
+        
+        if (sent < 0) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                cerr << "Таймаут отправки сообщения\n";
+                return false;
+            } else {
+                perror("Ошибка отправки");
+                return false;
+            }
+        }
+        total_sent += sent;
+    }
+    
+    return total_sent == message.length();
+}
+
+// Безопасное получение
+string safe_receive(int socket, size_t buffer_size, int timeout_sec = 5) {
+    if (!set_socket_timeout(socket, timeout_sec)) {
+        return "";
+    }
+    
+    vector<char> buffer(buffer_size, 0);
+    ssize_t received = read(socket, buffer.data(), buffer_size - 1);
+    
+    if (received < 0) {
+        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+            cerr << "Таймаут ожидания ответа от сервера\n";
+            return "";
+        } else {
+            perror("Ошибка чтения");
+            return "";
+        }
+    } else if (received == 0) {
+        cout << "Сервер отключился\n";
+        return "";
+    }
+    
+    buffer[received] = '\0';
+    return string(buffer.data());
+}
+
+// Поток для приема сообщений от сервера
+void receiver_thread(int sock, size_t buffer_size, int timeout_sec) {
+    cout << "Поток приема запущен\n";
+    
+    while (running) {
+        string response = safe_receive(sock, buffer_size, timeout_sec);
+        
+        if (response.empty()) {
+            if (running) {
+                cerr << "Потеряно соединение с сервером\n";
+            }
+            running = false;
+            break;
+        }
+        
+        cout << "\nСервер: " << response << endl;
+        cout << "Вы: " << flush;
+    }
+    
+    cout << "Поток приема завершен\n";
+}
+
+int connect_to_server(uint16_t port, const string &ip, int timeout_sec = 5) {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0)
-    {
-        std::cerr << "Не удалось создать сокет\n";
+    if (sock < 0) {
+        cerr << "Не удалось создать сокет\n";
+        return -1;
+    }
+
+    // Устанавливаем таймаут на подключение
+    if (!set_socket_timeout(sock, timeout_sec)) {
+        close(sock);
         return -1;
     }
 
@@ -22,88 +130,94 @@ int connect_to_server(uint16_t port, const string &ip)
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(port);
 
-    if (inet_pton(AF_INET, ip.c_str(), &serv_addr.sin_addr) <= 0)
-    {
-        std::cerr << "Неверный IP-адрес\n";
+    if (inet_pton(AF_INET, ip.c_str(), &serv_addr.sin_addr) <= 0) {
+        cerr << "Неверный IP-адрес\n";
         close(sock);
         return -1;
     }
 
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+            cerr << "Таймаут подключения к серверу\n";
+        } else {
+            perror("Ошибка подключения");
+        }
         close(sock);
-        return -1; // подключение не удалось
+        return -1;
     }
 
     return sock;
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+    std::signal(SIGINT, signal_handler);
+    std::signal(SIGPIPE, SIG_IGN);
+
     auto conf = load_config_from_args(argc, argv);
 
     size_t buffer_size = conf["buffer size"].get<size_t>();
     uint16_t port = conf["port"].get<uint16_t>();
     string ip = conf["ip"].get<string>();
-    int delay = conf["delay"].get<int>();
+    int delay = conf.value("delay", 3);
+    int timeout_sec = conf.value("timeout", 5);
+    int connect_timeout = conf.value("connect_timeout", 5);
 
-    std::cout << "Клиент запущен. Подключение к " << ip << ":" << port << "...\n";
+    cout << "Клиент запущен. Подключение к " << ip << ":" << port << "...\n";
+    cout << "Таймаут операций: " << timeout_sec << " сек.\n";
+    cout << "Таймаут подключения: " << connect_timeout << " сек.\n";
+    cout << "Введите сообщение или '/quit' для выхода:\n";
 
     int sock = -1;
 
-    // Основной цикл: переподключение при необходимости
-    while (true)
-    {
-        if (sock == -1)
-        {
-            sock = connect_to_server(port, ip);
-            if (sock == -1)
-            {
-                std::cout << "Не удаётся подключиться к серверу. Повтор через "
-                          << delay << " сек...\n";
-                std::this_thread::sleep_for(std::chrono::seconds(delay));
+    while (running) {
+        if (sock == -1) {
+            sock = connect_to_server(port, ip, connect_timeout);
+            if (sock == -1) {
+                cout << "Не удаётся подключиться к серверу. Повтор через " 
+                     << delay << " сек...\n";
+                sleep(delay);
                 continue;
             }
-            std::cout << "Подключено к серверу!\n";
+            cout << "Подключено к серверу!\n";
+            
+            // Запускаем поток для приема сообщений
+            thread receiver(receiver_thread, sock, buffer_size, timeout_sec);
+            receiver.detach();
         }
-        // Обмен сообщениями
-        std::string message;
-        std::cout << "Вы: ";
-        if (!std::getline(std::cin, message))
-        {
-            std::cout << "\nЗавершение работы по запросу пользователя.\n";
+
+        // Ввод сообщения от пользователя
+        string message;
+        cout << "Вы: ";
+        if (!getline(cin, message)) {
+            cout << "\nЗавершение работы\n";
             break;
         }
 
-        if (message.empty())
+        if (message.empty()) {
             continue;
-
-        // Отправка
-        if (send(sock, message.c_str(), message.length(), 0) <= 0)
-        {
-            std::cerr << "Ошибка отправки. Сервер, возможно, отключён.\n";
-            close(sock);
-            sock = -1;
-            continue; // вернёмся к попытке переподключения
         }
 
-        // Приём ответа
-        vector<char> buffer(buffer_size, {});
-        int valread = read(sock, buffer.data(), sizeof(buffer) - 1);
-        if (valread <= 0)
-        {
-            std::cout << "Сервер разорвал соединение. Попытка переподключения...\n";
+        if (message == "/quit") {
+            cout << "Завершение работы\n";
+            break;
+        }
+
+        // Отправка сообщения
+        if (!safe_send(sock, message, timeout_sec)) {
+            cerr << "Ошибка отправки. Переподключение...\n";
             close(sock);
             sock = -1;
             continue;
         }
 
-        buffer[valread] = '\0';
-        std::cout << "Сервер: " << buffer.data() << std::endl;
+        cout << "Сообщение отправлено. Ожидание ответа...\n";
     }
 
-    if (sock != -1)
+    running = false;
+    if (sock != -1) {
         close(sock);
+    }
 
+    cout << "Клиент остановлен.\n";
     return 0;
 }
--- TCP/server/server.cpp
+++ TCP/server/server.cpp
@@ -7,99 +7,235 @@
 #include <config.hpp>
 #include <vector>
 #include <csignal>
+#include <thread>
+#include <atomic>
+#include <poll.h>
+#include <sys/timerfd.h>
+#include <sys/time.h>
 
 using namespace std;
 
-volatile bool running = true;
+atomic<bool> running{true};
 
-void signal_handler(int sig)
-{
+void signal_handler(int sig) {
     running = false;
-    std::cout << "\nПолучен сигнал завершения. Завершение работы...\n";
-    exit(0);
+    cout << "\nПолучен сигнал завершения. Завершение работы...\n";
 }
 
-int main(int argc, char **argv)
-{
+// Функция для установки таймаута на сокет
+bool set_socket_timeout(int sockfd, int seconds) {
+    struct timeval timeout;
+    timeout.tv_sec = seconds;
+    timeout.tv_usec = 0;
+    
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0) {
+        perror("setsockopt SO_RCVTIMEO failed");
+        return false;
+    }
+    
+    if (setsockopt(sockfd, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(timeout)) < 0) {
+        perror("setsockopt SO_SNDTIMEO failed");
+        return false;
+    }
+    
+    return true;
+}
+
+// Функция для безопасной отправки данных
+bool safe_send(int socket, const string& message, int timeout_sec = 5) {
+    if (!set_socket_timeout(socket, timeout_sec)) {
+        return false;
+    }
+    
+    size_t total_sent = 0;
+    while (total_sent < message.length() && running) {
+        ssize_t sent = send(socket, message.c_str() + total_sent, 
+                           message.length() - total_sent, MSG_NOSIGNAL);
+        
+        if (sent < 0) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                cerr << "Таймаут отправки сообщения\n";
+                return false;
+            } else if (errno == EPIPE) {
+                cerr << "Клиент отключился\n";
+                return false;
+            } else {
+                perror("Ошибка отправки");
+                return false;
+            }
+        }
+        total_sent += sent;
+    }
+    
+    return total_sent == message.length();
+}
+
+// Функция для безопасного чтения данных
+string safe_receive(int socket, size_t buffer_size, int timeout_sec = 5) {
+    if (!set_socket_timeout(socket, timeout_sec)) {
+        return "";
+    }
+    
+    vector<char> buffer(buffer_size, 0);
+    ssize_t total_received = 0;
+    
+    // Сначала читаем длину сообщения (если используется протокол с длиной)
+    ssize_t received = read(socket, buffer.data(), buffer_size - 1);
+    
+    if (received < 0) {
+        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+            cerr << "Таймаут ожидания сообщения\n";
+            return "";
+        } else {
+            perror("Ошибка чтения");
+            return "";
+        }
+    } else if (received == 0) {
+        cout << "Клиент отключился\n";
+        return "";
+    }
+    
+    buffer[received] = '\0';
+    return string(buffer.data());
+}
+
+// Поток для чтения сообщений от клиента
+void client_receiver_thread(int client_socket, const string& client_ip, size_t buffer_size) {
+    cout << "Поток приема запущен для клиента " << client_ip << endl;
+    
+    while (running) {
+        string message = safe_receive(client_socket, buffer_size, 10);
+        
+        if (message.empty()) {
+            if (running) {
+                cerr << "Ошибка получения сообщения от клиента " << client_ip << endl;
+            }
+            break;
+        }
+        
+        cout << "Клиент " << client_ip << ": " << message << endl;
+        
+        // Эхо-ответ (можно убрать если не нужно)
+        if (!safe_send(client_socket, "Эхо: " + message)) {
+            break;
+        }
+    }
+    
+    close(client_socket);
+    cout << "Поток приема завершен для клиента " << client_ip << endl;
+}
+
+// Поток для отправки сообщений клиенту
+void client_sender_thread(int client_socket, const string& client_ip) {
+    cout << "Поток отправки запущен для клиента " << client_ip << endl;
+    
+    while (running) {
+        string message;
+        cout << "Сервер (для " << client_ip << "): ";
+        
+        if (!getline(cin, message)) {
+            if (running) {
+                cout << "Ввод прерван\n";
+            }
+            break;
+        }
+        
+        if (message.empty()) {
+            continue;
+        }
+        
+        if (message == "/quit") {
+            cout << "Завершение работы с клиентом " << client_ip << endl;
+            running = false;
+            break;
+        }
+        
+        if (!safe_send(client_socket, message)) {
+            cerr << "Не удалось отправить сообщение клиенту " << client_ip << endl;
+            break;
+        }
+        
+        cout << "Сообщение отправлено клиенту " << client_ip << endl;
+    }
+    
+    close(client_socket);
+    cout << "Поток отправки завершен для клиента " << client_ip << endl;
+}
+
+int main(int argc, char **argv) {
     signal(SIGINT, signal_handler);
+    signal(SIGPIPE, SIG_IGN); // Игнорировать SIGPIPE
 
     auto conf = load_config_from_args(argc, argv);
 
     size_t buffer_size = conf["buffer size"].get<size_t>();
     uint16_t port = conf["port"].get<uint16_t>();
+    int timeout_sec = conf.value("timeout", 5);
 
-    int server_fd, new_socket;
+    int server_fd;
     sockaddr_in address;
     int addrlen = sizeof(address);
 
     // Создание сокета
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         return -1;
     }
 
-    // Разрешить повторное использование адреса (чтобы не ждать TIME_WAIT)
+    // Разрешить повторное использование адреса
     int opt = 1;
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        perror("setsockopt SO_REUSEADDR failed");
+        close(server_fd);
+        return -1;
+    }
 
     // Привязка сокета к порту
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(port);
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
-    {
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("bind failed");
         close(server_fd);
         return -1;
     }
 
     // Прослушивание порта
-    if (listen(server_fd, 3) < 0)
-    {
+    if (listen(server_fd, 3) < 0) {
         perror("listen failed");
         close(server_fd);
         return -1;
     }
 
-    std::cout << "Сервер запущен на порту " << port << ". Ожидание подключения...\n";
+    cout << "Сервер запущен на порту " << port << ". Ожидание подключения...\n";
+    cout << "Таймаут операций: " << timeout_sec << " сек.\n";
+    cout << "Введите '/quit' для завершения работы.\n";
 
-    while (running)
-    {
-        int new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-        if (new_socket < 0)
-        {
-            if (!running)
-                break; // прервано сигналом
+    while (running) {
+        int client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+        if (client_socket < 0) {
+            if (!running) break;
             perror("accept failed");
+            continue;
         }
 
-        std::cout << "Новое подключение: " << inet_ntoa(address.sin_addr) << std::endl;
+        string client_ip = inet_ntoa(address.sin_addr);
+        cout << "Новое подключение: " << client_ip << endl;
 
-        vector<char> buffer(buffer_size, {});
-        while (running)
-        {
-            memset(buffer.data(), 0, buffer_size);
-            int valread = read(new_socket, buffer.data(), buffer_size - 1);
-            if (valread <= 0)
-            {
-                if (valread < 0)
-                    perror("read error");
-                else
-                    std::cout << "Клиент отключился.\n";
-                break;
-            }
-            buffer[valread] = '\0';
-            std::cout << "От клиента: " << buffer.data() << std::endl;
-            send(new_socket, buffer.data(), strlen(buffer.data()), 0);
-        }
+        // Запускаем потоки для общения с клиентом
+        thread receiver_thread(client_receiver_thread, client_socket, client_ip, buffer_size);
+        thread sender_thread(client_sender_thread, client_socket, client_ip);
 
-        close(new_socket);
-        std::cout << "Соединение закрыто. Ожидание нового клиента...\n";
+        // Ждем завершения потоков
+        sender_thread.join();
+        receiver_thread.join();
+
+        cout << "Соединение с клиентом " << client_ip << " закрыто.\n";
+        cout << "Ожидание нового подключения...\n";
     }
 
     close(server_fd);
-    std::cout << "Сервер остановлен.\n";
+    cout << "Сервер остановлен.\n";
     return 0;
 }
